af5d192ebf60f46a83a6fe9194aa449cdd500801
woodmawa <will.woodman@btinternet.com>
2024-12-02 12:55:59 +0000

    added a deploy controller endpoint  and service

--- src/main/groovy/com/flowable/restControllers/ProcessRestController.groovy
+++ src/main/groovy/com/flowable/restControllers/ProcessRestController.groovy
@@ -1,12 +1,15 @@
 package com.flowable.restControllers
 
 import com.flowable.services.AppProcessService
+import com.flowable.services.DeployProcessService
 import com.flowable.services.ProcessVariableService
 import groovy.util.logging.Slf4j
 import org.flowable.engine.runtime.ProcessInstance
 import org.flowable.task.api.Task
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,9 +17,11 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
-@RestController
+
 @Slf4j
+@RestController @RequestMapping("/api/process")
 class ProcessRestController {
     @Autowired
     private AppProcessService processService
@@ -24,7 +29,24 @@ class ProcessRestController {
     @Autowired private
     ProcessVariableService processVariableService
 
-    @PostMapping(value="/startProcess")
+    @Autowired
+    private DeployProcessService deployProcessService
+
+    @PostMapping("/deploy")
+    public ResponseEntity<String> deployProcess(@RequestParam("file") MultipartFile file) {
+        try {
+            boolean deployed = deployProcessService.deployProcessDefinition(file)
+            if (deployed) {
+                return ResponseEntity.ok("Process deployed successfully.")
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to deploy process.")
+            }
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing the file.")
+        }
+    }
+
+    @PostMapping(value="/start")
     public String startProcessInstance(@RequestParam("name") String processName, @RequestParam("variable") String value) {
 
         if (processService.checkProcessExists(processName)) {
@@ -40,7 +62,7 @@ class ProcessRestController {
         }
     }
 
-    @GetMapping("/processVariables") public Map<String, Object> getProcessVariables (@RequestParam ("pid") String processInstanceId) {
+    @GetMapping("/variables") public Map<String, Object> getProcessVariables (@RequestParam ("pid") String processInstanceId) {
         log.debug "reading process variables for pid $processInstanceId"
         return processVariableService.getProcessVariablesFromFinishedProcess(processInstanceId)
     }
@@ -50,8 +72,10 @@ class ProcessRestController {
     }
 
     @RequestMapping(value="/hello", method= RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
-    void sayHello (@RequestParam("name") String name) {
-        println "hello  " + name
+    String sayHello (@RequestParam("name") String name) {
+        String result = "hello  " + name
+        println result
+        result
     }
 
     @RequestMapping(value="/tasks", method= RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
