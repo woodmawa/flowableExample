3f4bdbbc2f4ceb4dde6256edaf3a58d34513c2bb
woodmawa <will.woodman@btinternet.com>
2024-12-02 18:20:21 +0000

    reads admin pass from env variable

--- src/main/groovy/com/flowable/security/FlowableConfig.groovy
+++ src/main/groovy/com/flowable/security/FlowableConfig.groovy
@@ -2,11 +2,16 @@ package com.flowable.security
 
 import groovy.util.logging.Slf4j
 import jakarta.annotation.PostConstruct
+import org.apache.tomcat.util.IntrospectionUtils
 import org.flowable.engine.IdentityService;
 import org.flowable.idm.api.Group;
 import org.flowable.idm.api.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @Slf4j
@@ -15,12 +20,18 @@ public class FlowableConfig {
     @Autowired
     private IdentityService identityService
 
+    //read default password from system properties - set this on your machine
+    @Value ('${FLOWABLE_ADMIN_PASSWORD:admin}')
+    private String adminPassword
+
     @PostConstruct  //auto start the init routine
     public void init() {
+
         if (identityService.createUserQuery().userId("admin").singleResult() == null) {
             log.info ("FlowableUserAndRolesConfig:  adding admin user with default password ")
             User admin = identityService.newUser("admin")
-            admin.setPassword("admin")
+            assert adminPassword
+            admin.setPassword(adminPassword)
             identityService.saveUser(admin)
 
             Group adminGroup = identityService.newGroup("adminGroup")
@@ -34,4 +45,6 @@ public class FlowableConfig {
             log.info "FlowableUserAndRolesConfig: noop - admin user id already created "
         }
     }
+
+
 }
